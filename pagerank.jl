e46f19a3f239c41c4998cf9d461e1b9b52c2290a
rezvanr2378@gmail.com <rezvanr2378@gmail.com>
2022-09-22 21:21:22 +0200

    Add comments and clean up

--- pagerank.jl
+++ pagerank.jl
@@ -1,6 +1,7 @@
 using DiscreteMarkovChains, BenchmarkTools
 
-function check_regularity(P)
+# Checks if a given matrix is regular or not
+function check_regularity(P::Matrix)
 Pn = P
         for i in 1:100
                 Pn *= P
@@ -12,6 +13,7 @@ Pn = P
         end
 end
 
+# get the q vector which solves q=qP
 function get_q(P::Matrix)::Vector{Float64}
         Q = P
                 for i in 1:1000
@@ -21,6 +23,7 @@ function get_q(P::Matrix)::Vector{Float64}
         return q
 end
 
+# get the q vector wich solves q=qP, where P is the Pagerank matrix, input is a adjacency matrix and λ
 function get_q_pagerank_matrix(A::Matrix, λ::Float64)::Vector
         sz = size(A)
         n = sz[1]
@@ -45,6 +48,7 @@ function get_q_pagerank_matrix(A::Matrix, λ::Float64)::Vector
         return q
 end
 
+# Simulates a random surfer which takes num_steps of steps in a given Stochastic/Markov matrix
 function random_surfer(P_ex::Matrix, num_steps::Integer)
         sz = size(P_ex)
         max = sz[1]
@@ -62,29 +66,46 @@ function random_surfer(P_ex::Matrix, num_steps::Integer)
         end
 end
 
+# A adjacency matrix which represent the relationship between different pages
 A_ex = [ 0 1 1 1 
         0 0 1 1
         1 0 0 0
         1 0 1 0 ]
 
+# Another example adjacency matrix
 G_ex = [ 1 1 1  
          0 1 1 
          1 0 1 ]
 
+# The Markov/Stochastic matrix, given from A_ex
 P_ex = [ 0 1/3 1/3 1/3 
         0 0 1/2 1/2
         1 0 0 0
         1/2 0 1/2 0 ]
 
+#  A example Pagerank matrix given a λ
 P_r = [   0.125  0.291667  0.291667  0.291667
           0.125  0.125     0.375     0.375
           0.625  0.125     0.125     0.125
           0.375  0.125     0.375     0.125]
-          
+
+
+# Dampening factor, which describes how "bored" the surfer is getting
 λ = 0.0
+
+#= For question 1(h)
+        u = [ 1/4 1/4 1/4 1/4 ]
+        P2 = P_ex^10
+        ans = u * P2
+        println(ans)
+=#
+
+#= 
+get_q(P_ex)
 q_res_example = get_q_pagerank_matrix(A_ex, λ)
 println(q_res_example)
 q_res_general = get_q_pagerank_matrix(G_ex, λ)
 println(q_res_general)
 random_surfer(P_ex, 10)
 check_regularity(P_ex)
+=#
